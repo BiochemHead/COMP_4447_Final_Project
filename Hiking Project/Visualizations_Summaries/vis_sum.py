b0e96fc8a0608cd910b3842c65bdab217b095753
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-05 05:50:15 -0700

    added random data for 17 vars

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -4,4 +4,23 @@ Data Started: 02-04-2023
 Author(s): Chris Kirchberg
 """
 import pandas as pd
+import numpy as np
 import matplotlib as plt
+
+df_columns=['hike_id', 'hike_name','type','dist','elev_gain', 'elev_loss','ave_grade',
+            'max_grade','difficulty','num_reviews','ave_rating','location','dogs',
+            'features','description','rating_dist','diff_dist']
+data=[]
+letters = np.array(list(chr(ord('a') + i) for i in range(26)))
+for i in range(5000):
+    data.append([i+100,np.random.choice(letters),
+                 np.random.choice(letters),np.random.uniform(1,25),
+                 np.random.uniform(1,2500),np.random.uniform(0,2500),
+                 np.random.uniform(0,100),np.random.uniform(0,100),
+                 np.random.randint(6),np.random.randint(100),
+                 np.random.randint(5),np.random.choice(letters),
+                 np.random.choice(letters),np.random.choice(letters),
+                 np.random.choice(letters),
+                 np.random.uniform(0,100),np.random.uniform(0,100)])
+    
+hike_df = pd.DataFrame(data,columns=df_columns)
