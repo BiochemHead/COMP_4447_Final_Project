75f93839076fc257b9ee5b645522154870df8c00
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-05 06:45:52 -0700

    Added some notes about source of URLs

--- Hiking Project/bs4_scrape_w_idx.py
+++ Hiking Project/bs4_scrape_w_idx.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup as bsoup
 import requests
 
-urls=[]
 # idx ranges from 1 to 409 after first get to main url
+# there are only 4901 trails if you click through all of the show more buttons
+# Title says 5855 trails, but not all are shown/acceccable
+# From areas section that lists all states (https://www.hikingproject.com/directory/areas),
+# site say colorado has 5855. but from home page under Trail Directory
+# it says colorado has 6329 trails. Clearly some discrepancy.
+# Used network section of inspect elelments to find XHR section/traffic for ajax URL
+
+urls=[]
+
 for i in range(409):
     response = requests.get('https://www.hikingproject.com/ajax/area/8007418/trails?idx='+str(i+1))
     #print(response.status_code)
