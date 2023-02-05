c2ba8ee90bd47f3ed09e7b4c633801eb97143855
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-05 06:09:19 -0700

    added idx loop

--- Hiking Project/bs4_scrape_w_idx.py
+++ Hiking Project/bs4_scrape_w_idx.py
@@ -1,22 +1,22 @@
 from bs4 import BeautifulSoup as bsoup
 import requests
 
-response = requests.get('https://www.hikingproject.com/ajax/area/8007418/trails?idx=408')
+urls=[]
 # idx ranges from 1 to 408 after first get to main url
-
-print(response.status_code)
-#response.text
-soup = bsoup(response.text, 'lxml')
-
-if soup.tr== None:
-    print ('all done')
-else:
-    for trtag in soup.find_all('tr'):
-        print(trtag['data-href'])
+for i in range(408):
+    response = requests.get('https://www.hikingproject.com/ajax/area/8007418/trails?idx='+str(i+1))
+    #print(response.status_code)
+    soup = bsoup(response.text, 'lxml')
+    if soup.tr== None:
+        print ('all done')
+    else:
+        for trtag in soup.find_all('tr'):
+            urls.append([response.status_code,trtag['data-href']])
+            #print(trtag['data-href'])
     
-response = requests.get('https://www.hikingproject.com/trail/7038057/mf-ranch-creek-road-142')
-print(response.status_code)
+""" response2 = requests.get('https://www.hikingproject.com/trail/7038057/mf-ranch-creek-road-142')
+print(response2.status_code)
 #response.text
-soup = bsoup(response.text, 'lxml')
+soup2 = bsoup(response2.text, 'lxml') """
 
 #soup.find_all('div',
