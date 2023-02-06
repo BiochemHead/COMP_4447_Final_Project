0e0917c79c7eaef6c57730df98e1233b03220c3a
Ahmed Ayoubi <71293836+amido84@users.noreply.github.com>
2023-02-05 21:06:29 -0600

    V1.1
    1- fixed "show more" 
    2- pulled the hike trail name from each link and placed it in a data frame

--- Hiking Project/scrape_clean/scrape_a_hike.py
+++ Hiking Project/scrape_clean/scrape_a_hike.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-
-df = pd.DataFrame(columns=['hike_id', 'hike_name', 'hike_region', 'total_distance'])
+link_df = pd.DataFrame(columns=['hike_link']) 
+df = pd.DataFrame(columns=['hike_name']) 
 
 # open a web browser
 driver = webdriver.Chrome()
@@ -12,26 +12,44 @@ driver = webdriver.Chrome()
 # navigate to the page
 driver.get('https://www.hikingproject.com/search?q=colorado')
 
-# locate the elements that contain the hike name and region
-hike_ids = driver.find_elements(By.XPATH, "//div[@class='sc-dPWrhe digRqT']/a[1]")
-hike_names = driver.find_elements(By.XPATH, "//h3[contains(@class, 'sc-cjibBx cKMyqg')]")
-hike_regions = driver.find_elements(By.XPATH, "//div[contains(@class, 'sc-gYbzsP hksFOR')]")
-total_distances = driver.find_elements(By.XPATH, "//div[contains(@class, 'sc-cCjUiG gazdbj')]")
+# locate the "Load More" button
+load_more_button = driver.find_element(By.XPATH, "//button[text()='Load More']")
 
-# iterate through the list of elements and extract the hike name and region
-for i in range(min((len(hike_ids), len(hike_names), len(hike_regions), len(total_distances)))):
-    hike_id = hike_ids[i].get_attribute("href").split("/")[4]
-    hike_name = hike_names[i].text
-    hike_region = hike_regions[i].text.split(">")[0]
-    total_distance = total_distances[i].text
-    # append the extracted information to the DataFrame
-    df = pd.concat([df, pd.DataFrame({'hike_id': hike_id, 'hike_name': hike_name, 'hike_region': hike_region, 'total_distance': total_distance}, index=[len(df)])], ignore_index=False)
-   
-# print the DataFrame
-#print(df)
+# iterate 2 times and click the "Load More" button
+for i in range(2):
+    if load_more_button.is_displayed():
+        load_more_button.click()
 
+    # locate the elements that contain the hike name and region
+    hike_links = driver.find_elements(By.XPATH, "//div[@class='sc-dPWrhe digRqT']/a")
+
+    # iterate through the list of elements and extract the hike id
+    for i in range(len(hike_links)):
+        hike_link = hike_links[i].get_attribute("href")
+        
+        # append the extracted information to the DataFrame
+        link_df = pd.concat([link_df, pd.DataFrame({'hike_link': hike_link}, index=[len(link_df)])], ignore_index=False)
+     
+ # iterate over the links in link_df
+for i in range(len(link_df)):
+    hike_link = link_df.iloc[i]["hike_link"]
+    driver.get(hike_link)
+    hike_names = driver.find_elements(By.XPATH, '//h1[@id="trail-title"]')
+    if len(hike_names) == 0:
+         # if there are no elements with an id of 'trail-title', continue to the next iteration
+        continue
+    hike_name = hike_names[0].text
+    # create a new data frame with a single row containing the value of hike_name
+    new_df = pd.DataFrame({'hike_name': [hike_name]})
+    # concatenate the new data frame to the existing data frame hike_df
+    df = pd.concat([df, new_df], ignore_index=True)
+            
+            
 # close the web browser
 driver.quit()
+
+# print the DataFrame
+#print(link_df)
 
 # converting the dataframe object (df) to an HTML table
 html_table = df.to_html()
