7c2c2a00f16ab676ed386c72beef57a72953fab6
Ahmed Ayoubi <71293836+amido84@users.noreply.github.com>
2023-02-07 11:00:44 -0600

    V1.2
    This code is for scraping 3363 hiking trails in Colorado from "www.hikingproject.com"

--- Hiking Project/scrape_clean/scrape_a_hike.py
+++ Hiking Project/scrape_clean/scrape_a_hike.py
@@ -1,59 +1,82 @@
-# Scraping www.hikingproject.com for trails in Colorado
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+from IPython.display import display, HTML
 
-link_df = pd.DataFrame(columns=['hike_link']) 
-df = pd.DataFrame(columns=['hike_name']) 
-
+ 
 # open a web browser
 driver = webdriver.Chrome()
 
 # navigate to the page
-driver.get('https://www.hikingproject.com/search?q=colorado')
+driver.get('https://www.hikingproject.com/directory/8007418/colorado')
 
 # locate the "Load More" button
-load_more_button = driver.find_element(By.XPATH, "//button[text()='Load More']")
+load_more_button = driver.find_element(By.XPATH, "//button[@id='load-more-trails']")
 
-# iterate 2 times and click the "Load More" button
-for i in range(2):
+# iterate 3 times and click the " Show More" button
+for i in range(5):
     if load_more_button.is_displayed():
         load_more_button.click()
 
     # locate the elements that contain the hike name and region
-    hike_links = driver.find_elements(By.XPATH, "//div[@class='sc-dPWrhe digRqT']/a")
+    hike_ids = driver.find_elements(By.XPATH, "//table[@class='table table-striped trail-table']//tr/td/a") 
+ 
+    # Create alist for all the trail pages 
+    links =[]
 
-    # iterate through the list of elements and extract the hike id
-    for i in range(len(hike_links)):
-        hike_link = hike_links[i].get_attribute("href")
+    # iterate through the list of elements and extract the hike information
+    for j in range(len(hike_ids)):
+        hike_link = hike_ids[j].get_attribute("href")
+        links.append(hike_link)
+
+# locate the elements that contain the 'Trail Name','Trail Length','Trail Difficulty', 'Trail Rating', 'User Rate Number' 'Trail Type', 'Elev_Up', 'Elev_Down', 'Highest Elevation', 'Lowest Elevation', 'Average Grade', 'Max Grade', 'Dogs and Features'
+hike_list =[]
+
+for i in range (len(links)):
+    for link in links:
+        driver.get(link)
         
-        # append the extracted information to the DataFrame
-        link_df = pd.concat([link_df, pd.DataFrame({'hike_link': hike_link}, index=[len(link_df)])], ignore_index=False)
-     
- # iterate over the links in link_df
-for i in range(len(link_df)):
-    hike_link = link_df.iloc[i]["hike_link"]
-    driver.get(hike_link)
-    hike_names = driver.find_elements(By.XPATH, '//h1[@id="trail-title"]')
-    if len(hike_names) == 0:
-         # if there are no elements with an id of 'trail-title', continue to the next iteration
-        continue
-    hike_name = hike_names[0].text
-    # create a new data frame with a single row containing the value of hike_name
-    new_df = pd.DataFrame({'hike_name': [hike_name]})
-    # concatenate the new data frame to the existing data frame hike_df
-    df = pd.concat([df, new_df], ignore_index=True)
-            
-            
+        hike_name = driver.find_element(By.ID, "trail-title")
+        
+        hike_difficulty = [d.text for d in driver.find_elements(By.CLASS_NAME, 'trail-subheader')]
+        
+        hike_rating = [r.text for r in driver.find_elements(By.CSS_SELECTOR, '#title-stars > span.small')]
+        
+        no_userRate = [r.text for r in driver.find_elements(By.CSS_SELECTOR, '#title-stars > span.small')]
+        
+        dog_feature = [d.text for d in driver.find_elements(By.CSS_SELECTOR, '#trail-text > div:nth-child(2) > h3 > span')]
+        
+        description = driver.find_elements(By.CSS_SELECTOR, '#trail-text > div:nth-child(4)')
+        
+        stats = driver.find_elements(By.CLASS_NAME, 'stat-box')
+        
+        elev_up= driver.find_element(By.CSS_SELECTOR, "#trail-stats-bar > div:nth-child(3) > h3:nth-child(1) > span.imperial")
+        
+        elev_down= driver.find_element(By.CSS_SELECTOR, '#trail-stats-bar > div:nth-child(3) > h3:nth-child(3) > span.imperial')
+        
+        trail_length= driver.find_element(By.CSS_SELECTOR,'#trail-stats-bar > div.stat-block.ml-2.mr-1.mt-1 > span.imperial > h3')
+        
+        trail_type = driver.find_element(By.CSS_SELECTOR,'#trail-stats-bar > div.stat-block.ml-2.mr-1.mt-1 > h3')
+        
+        elev_high = driver.find_element(By.CSS_SELECTOR, '#trail-stats-bar > div:nth-child(2) > h3:nth-child(1) > span.imperial')
+        
+        elev_low = driver.find_element(By.CSS_SELECTOR, '#trail-stats-bar > div:nth-child(2) > h3:nth-child(3) > span.imperial')
+        
+        avg_grade = driver.find_element(By.CSS_SELECTOR, '#trail-stats-bar > div.stat-block.ml-1.mt-1 > h3:nth-child(1)')
+        
+        max_grade = driver.find_element(By.CSS_SELECTOR, '#trail-stats-bar > div.stat-block.ml-1.mt-1 > h3:nth-child(3)')
+ 
+        
+        hike_list.append([hike_name.text, trail_length.text, hike_difficulty, hike_rating, no_userRate, trail_type.text, elev_up.text, elev_down.text, elev_high.text, elev_low.text, avg_grade.text, max_grade.text, dog_feature, [stat.text for stat in stats]])
+
+        
+        df = pd.DataFrame(data=hike_list, columns=['Trail Name', 'Trail Length','Trail Difficulty', 'Trail Rating', 'User Rate Number', 'Trail Type', 'Elev_Up', 'Elev_Down', 'Highest Elevation', 'Lowest Elevation', 'Average Grade', 'Max Grade', 'Dogs and Features', 'Stats Info.'])
+        hike_df = df.replace('\n', ' ', regex=True)
+        
 # close the web browser
 driver.quit()
 
-# print the DataFrame
-#print(link_df)
-
-# converting the dataframe object (df) to an HTML table
-html_table = df.to_html()
-with open('table.html', 'w') as f:
-    f.write(html_table)
-from IPython.display import display, HTML
-display(HTML(df.to_html()))
+display(HTML(hike_df.to_html()))
