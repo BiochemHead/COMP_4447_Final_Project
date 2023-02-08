fdb5eace2bb764b267ca5a6f33e4e62e9e2528da
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-08 05:04:12 -0700

    added lines to read pkl file and make summary info

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -7,20 +7,8 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-df_columns=['hike_id', 'hike_name','type','dist','elev_gain', 'elev_loss','ave_grade',
-            'max_grade','difficulty','num_reviews','ave_rating','location','dogs',
-            'features','description','rating_dist','diff_dist']
-data=[]
-letters = np.array(list(chr(ord('a') + i) for i in range(26)))
-for i in range(5000):
-    data.append([i+100,np.random.choice(letters),
-                 np.random.choice(letters),np.random.uniform(1,25),
-                 np.random.uniform(1,2500),np.random.uniform(0,2500),
-                 np.random.uniform(0,100),np.random.uniform(0,100),
-                 np.random.randint(6),np.random.randint(100),
-                 np.random.randint(5),np.random.choice(letters),
-                 np.random.choice(letters),np.random.choice(letters),
-                 np.random.choice(letters),
-                 np.random.uniform(0,100),np.random.uniform(0,100)])
-    
-hike_df = pd.DataFrame(data,columns=df_columns)
+# Read the cleaned hiking dataset pickle file  
+df = pd.read_pickle("hike_project_cln_data.pkl")
+
+df.info()
+df.details()
