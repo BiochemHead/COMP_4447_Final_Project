f949509526cc09563a950756ca1629259f3c3a3b
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-08 07:58:44 -0700

    Init plots and histograms

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -6,11 +6,15 @@ Author(s): Chris Kirchberg
 import pandas as pd
 import numpy as np
 import matplotlib as plt
+import pprint
 
 # Read the cleaned hiking dataset pickle file  
 df = pd.read_pickle("../scrape_clean/hike_project_cln_data.pkl")
 
 df.info()
 df.describe()
-df['Dogs'].value_counts()
-df['Trail Difficulty'].value_counts()
+df['Dogs'].value_counts().to_frame()
+df['Trail Difficulty'].value_counts().to_frame()
+df.hist()
+df.plot.scatter(x='Max Grade',y='Difficulty Number')
+df.plot.scatter(x='Difficulty Number',y='Trail Rating')
