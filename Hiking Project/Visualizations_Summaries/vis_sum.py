6ccddb319afb11f5c0efc58b3bc338e4b26f445f
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-08 11:55:54 -0700

    updated to keep some summ tables using display()

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -6,15 +6,17 @@ Author(s): Chris Kirchberg
 import pandas as pd
 import numpy as np
 import matplotlib as plt
-import pprint
+import seaborn as sb
+from sklearn import linear_model
+import statsmodels.api as sm
 
 # Read the cleaned hiking dataset pickle file  
 df = pd.read_pickle("../scrape_clean/hike_project_cln_data.pkl")
 
 df.info()
-df.describe()
-df['Dogs'].value_counts().to_frame()
-df['Trail Difficulty'].value_counts().to_frame()
+display(df.describe())
+display(df['Dogs'].value_counts(dropna=False).to_frame())
+display(df['Trail Difficulty'].value_counts(dropna=False).to_frame())
 df.hist()
 df.plot.scatter(x='Max Grade',y='Difficulty Number')
 df.plot.scatter(x='Difficulty Number',y='Trail Rating')
