8832cba0aca874f41cf09499a1ec73ff34636ca3
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-08 05:58:03 -0700

    changed a few of dtypes and create sep dog column

--- Hiking Project/scrape_clean/DataClean_a_hike.py
+++ Hiking Project/scrape_clean/DataClean_a_hike.py
@@ -5,10 +5,12 @@ import numpy as np
 df = pd.read_pickle("hike_project.pkl")
 
 # Showing Datatypes 
-df.dtypes
+#df.dtypes
 
 # Correcting DataTypes 
+df['Trail Name'] = df['Trail Name'].astype("string")
 df['Trail Length'] = df['Trail Length'].astype(float)
+df['Trail Type'] = df['Trail Type'].astype("string")
 df['Elev_Up'] = df['Elev_Up'].str.replace("'", "").str.replace(",", "").astype(float)
 df['Elev_Down'] = df['Elev_Down'].str.replace("'", "").str.replace(",", "").astype(float)
 df['Highest Elevation'] = df['Highest Elevation'].str.replace("'", "").str.replace(",", "").astype(float)
@@ -16,12 +18,11 @@ df['Lowest Elevation'] = df['Lowest Elevation'].str.replace("'", "").str.replace
 df['Average Grade'] = df['Average Grade'].str.strip('%').astype(float) / 100
 df['Max Grade'] = df['Max Grade'].str.strip('%').astype(float) / 100
 
-df.dtypes
-
 # Data cleaning on Trail Difficulty, Dogs and Features, Trail Rating, and User Rate Number
 df['Trail Difficulty'] = df['Trail Difficulty'].apply(lambda x: x[0].split()[0])
-df['Dogs and Features'] = df['Dogs and Features'].apply(lambda x: x[0].split()[0])
-df['Dogs and Features'] = df['Dogs and Features'].replace("Unknown", np.nan)
+df['Dogs'] = df['Dogs and Features'].apply(lambda x: x[0].split()[0])
+df['Dogs'] = df['Dogs'].replace("Unknown", np.nan)
+df['Dogs'] = df['Dogs'].astype("string")
 
 if not np.issubdtype(df['Trail Rating'].dtype, np.dtype(str).type):
     df['Trail Rating'] = df['Trail Rating'].astype(str)
@@ -30,6 +31,10 @@ df['Trail Rating'] = pd.to_numeric(df['Trail Rating'].str.extract('(\d+\.\d+)')[
 if not np.issubdtype(df['User Rate Number'].dtype, np.dtype(str).type):
     df['User Rate Number'] = df['User Rate Number'].astype(str)
 df['User Rate Number'] = df['User Rate Number'].str.extract('\((\d+)\)')[0]
+
+# Convert data type to int
+df['User Rate Number'] = df['User Rate Number'].astype('Int64')
+
 
 # Difficulty mapping
 difficulty_map = {
@@ -43,10 +48,14 @@ difficulty_map = {
 # Create the Difficulty Number column
 df['Difficulty Number'] = df['Trail Difficulty'].map(difficulty_map)
 
+# Convert datatype to string
+df['Trail Difficulty'] = df['Trail Difficulty'].astype("string")
+
 # Specify the desired order of columns
 columns = df.columns.tolist()
 columns.remove('Difficulty Number')
-columns = columns[:3] + ['Difficulty Number'] + columns[3:]
+columns.remove('Dogs')
+columns = columns[:3] + ['Difficulty Number'] + columns[3:12] + ['Dogs'] + columns[12:]
 df = df[columns]
 
 df.sample(5)
