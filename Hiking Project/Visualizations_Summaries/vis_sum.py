c452e2895cad2b1a51bf1e8b2b3754ec5fa1b47d
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-09 09:46:23 -0700

    changed to boxplot and agg plots

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -6,30 +6,41 @@ Author(s): Chris Kirchberg
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sb
-from sklearn import linear_model
-import statsmodels.api as sm
+import seaborn as sns
 
 # Read the cleaned hiking dataset pickle file  
-df = pd.read_pickle("../scrape_clean/hike_project_cln_data.pkl")
+df_o = pd.read_pickle("../scrape_clean/hike_project_cln_data.pkl")
+df=df_o.copy(deep=True)
 for i in range(len(df)):
     if pd.isna(df.loc[i,'Features']) is False:
         df['Features'][i]=df['Features'][i].split(sep ='·')
-        
+
+df['Difficulty Number']=df['Difficulty Number'].astype('category')
+
 df.info()
 display(df.describe())
+
+df.hist()
+plt.show()
 
 df['Dogs'].value_counts(dropna=False).plot.bar()
 plt.show()
 
-df['Trail Difficulty'].value_counts(dropna=False).plot.bar()
+df['Trail Difficulty'].value_counts(dropna=False).sort_index().plot.bar()
 plt.show()
 
 df['Features'].value_counts(dropna=False).plot.bar()
 plt.show()
 
 df.hist()
+plt.show()
 
-df.plot.scatter(x='Max Grade',y='Difficulty Number')
+plt.subplot(221)
+sns.boxplot(x='Difficulty Number',y='Trail Rating',data=df)
 
-df.plot.scatter(x='Difficulty Number',y='Trail Rating')
+plt.subplot(222)
+sns.regplot(x='Max Grade',y='Difficulty Number',data=df,dropna = True)
+
+plt.subplot(223)
+sns.regplot(x='Max Grade',y='Trail Rating',data=df,dropna = True)
+plt.show()
