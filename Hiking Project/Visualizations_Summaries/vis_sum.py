88135419195510bfe664023b6774b9f77deb6416
Chris Kirchberg <chris.kirchberg@du.edu>
2023-02-10 10:19:41 -0700

    Updated how plots are defined

--- Hiking Project/Visualizations_Summaries/vis_sum.py
+++ Hiking Project/Visualizations_Summaries/vis_sum.py
@@ -12,18 +12,19 @@ from IPython.core.display import display
 
 # Read the cleaned hiking dataset pickle file
 df_o = pd.read_pickle("../scrape_clean/hike_project_cln_data.pkl")
-df = df_o.copy(deep=True)
-
+df=df_o.copy(deep=True)
 df.replace({'Features':' · '},',',regex=True,inplace=True)
+df['Difficulty Number']=df['Difficulty Number'].astype('category')
 
-df['Difficulty Number'] = df['Difficulty Number'].astype('category')
-
+# Get basic information about the data
 df.info()
-display(df.describe())
+df.describe()
 
-plt.rcParams["figure.figsize"] = [9, 6]
-plt.rcParams["figure.autolayout"] = True
+# Plot count data of Dogs and Trail Difficulty on Bar Graphs
+plt.rcParams["figure.figsize"] = [9, 5]
+
 ax = plt.GridSpec(2, 1)
+ax.update(wspace=0.25, hspace=0.25)
 
 ax1 = plt.subplot(ax[0, 0])
 sns.countplot(y=df['Dogs'])
@@ -34,7 +35,9 @@ sns.countplot(y=df['Trail Difficulty'],
                      'INTERMEDIATE/DIFFICULT' ,'DIFFICULT'])
 plt.show()
 
-plt.rcParams["figure.figsize"] = [9, 5]
+# Plot count data of Features on Bar Graphs
+plt.rcParams["figure.figsize"] = [9, 9]
+ax.update(wspace=0.25, hspace=0.25)
 my_colors = list(islice(cycle(['b', 'r', 'g', 'y', 'c','m']), None, len(df)))
 df['Features'].value_counts(dropna=True).plot.barh(color=my_colors)
 plt.xlabel('Count')
@@ -42,17 +45,23 @@ plt.ylabel('Features')
 plt.title('Count of Features')
 plt.show()
 
-plt.rcParams["figure.figsize"] = [9, 6]
+# Plot count data of numeric columns on Histograms
+plt.rcParams["figure.figsize"] = [9, 9]
 df.hist()
 plt.show()
 
-plt.rcParams["figure.figsize"] = [9, 6]
-plt.subplot(221)
-sns.boxplot(x='Difficulty Number', y='Trail Rating', data=df)
+# Plot Box Plot for Difficulty Number vs. Trail Rating
+plt.rcParams["figure.figsize"] = [9, 9]
+ax = plt.GridSpec(2, 2)
 
-plt.subplot(222)
-sns.regplot(x='Max Grade', y='Difficulty Number', data=df, dropna=True)
+ax1 = plt.subplot(ax[0, 0])
+sns.boxplot(x='Difficulty Number',y='Trail Rating',data=df)
 
-plt.subplot(223)
-sns.regplot(x='Max Grade', y='Trail Rating', data=df, dropna=True)
+# Plot regression lines and scatter plots for Max Grade vs.
+# Difficulty Number and Trail Rating
+ax2 = plt.subplot(ax[1, 1])
+sns.regplot(x='Max Grade',y='Difficulty Number',data=df,dropna = True)
+
+ax3 = plt.subplot(ax[1, 0])
+sns.regplot(x='Max Grade',y='Trail Rating',data=df,dropna = True)
 plt.show()
